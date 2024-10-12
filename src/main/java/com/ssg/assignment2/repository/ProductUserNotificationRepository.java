bac11586a95479302926b006ddc238fba62d9eb6
polar7bear <sons1998@naver.com>
2024-10-12 20:25:13 +0900

    Fix: 알림등록 한 순서대로 알림을 보내주기 위해 등록날짜 오름차순으로 조회

--- src/main/java/com/ssg/assignment2/repository/ProductUserNotificationRepository.java
+++ src/main/java/com/ssg/assignment2/repository/ProductUserNotificationRepository.java
@@ -3,9 +3,19 @@ package com.ssg.assignment2.repository;
 import com.ssg.assignment2.entity.ProductUserNotification;
 import com.ssg.assignment2.entity.status.NotificationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductUserNotificationRepository extends JpaRepository<ProductUserNotification, Long> {
-    List<ProductUserNotification> findByProductIdAndNotificationStatus(Long productId, NotificationStatus notificationStatus);
+
+    @Query(
+            "SELECT pun " +
+                    "FROM ProductUserNotification pun " +
+                    "WHERE pun.product.id = :productId " +
+                    "AND pun.notificationStatus = :status " +
+                    "ORDER BY pun.createdAt ASC"
+    )
+    List<ProductUserNotification> findByProductIdAndNotificationStatus(Long productId, @Param("status") NotificationStatus notificationStatus);
 }
